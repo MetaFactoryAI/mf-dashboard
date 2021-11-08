0e8db84346721fb980117d239b13ca406562665e
xivanc <xivanc01@gmail.com>
2021-11-08 17:07:21 +0100

    Add eslint rules + prettier

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,16 +1,18 @@
-import { Box } from "@chakra-ui/layout";
-import Image from "next/image";
-import Link from "next/link";
-import React from "react";
+import { Box } from '@chakra-ui/layout';
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
 
-import styles from "../../styles/Header.module.css";
+import styles from '../../styles/Header.module.css';
 
-export const Header = () => (
+const Header = () => (
   <Box className={styles.header}>
     <Link href="/">
-      <a className={styles.logo}>
+      <a className={styles.logo} href="https://www.metafactory.ai/">
         <Image src="/header-logo-mf.svg" alt="" width="48px" height="48px" />
       </a>
     </Link>
   </Box>
 );
+
+export default Header;

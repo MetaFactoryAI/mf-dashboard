a8379198d4350d3b2b63e015dad84ddfed09ea46
xivanc <xivanc01@gmail.com>
2022-01-19 14:58:19 +0100

    update balancer const

--- utils/constants.ts
+++ utils/constants.ts
@@ -5,7 +5,6 @@ export const IPFS_CLAIMS_SNAPSHOT_URL =
   "storageapi.fleek.co/hammadj-team-bucket/robot-claim/snapshot.json";
 export const APP_NAME = "mf-dashboard";
 export const BALANCER_POOL_ID =
-  "0x3a19030ed746bd1c3f2b0f996ff9479af04c5f0a000200000000000000000004";
-export const BALANCER_GQL_URL =
-  "https://api.thegraph.com/subgraphs/name/balancer-labs/balancer-kovan-v2";
+  "0xccf5575570fac94cec733a58ff91bb3d073085c70002000000000000000000af";
+export const BALANCER_GQL_URL = "https://api.thegraph.com/subgraphs/name/balancer-labs/balancer-v2";
 export const METAFACTORY_GQL_URL = "https://metafactory.hasura.app/v1/graphql";

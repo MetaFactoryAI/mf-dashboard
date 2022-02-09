0b80eb770e7be1012cdec659c016f8aa7905aafc
xivanc <xivanc01@gmail.com>
2022-02-09 20:48:45 +0100

    fx

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,3 +1,4 @@
+// TODO: move constants to nextjs env file
 export const LOGO_HEIGHT = 64;
 export const MERKLE_REDEEM_CONTRACT = "0x67f5C948DceAf99352f6cac71c862C1C3b60dfDF";
 export const IPFS_NODE = "cloudflare-ipfs.com";

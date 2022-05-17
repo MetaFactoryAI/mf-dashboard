a62ffe5a070bffbcd73fdb3cc4b4952a94634198
xivanc <xivanc01@gmail.com>
2022-05-17 14:30:48 +0200

    Add test wearable assets; Add ListItem

--- components/closet/ClaimWearables.tsx
+++ components/closet/ClaimWearables.tsx
@@ -1,18 +1,25 @@
 /* eslint-disable prettier/prettier */
 import { Grid, GridItem } from "@chakra-ui/react";
 import type { NextPage } from "next";
+import ListItem from "./shared/ListItem";
 
 const ClaimWearables: NextPage = () => {
-  const texts = ["test1", "test2", "test3", "test4", "test5", "test6"]
+  const items = ["1", "2", "3", "4", "5", "6"]
 
   return (
-    <Grid templateColumns="repeat(10, 1fr)" width="100%" mt="25px">
-      { texts.map((text) =>
+    <Grid templateColumns="repeat(8, 1fr)">
+      { items.map((item) =>
       <GridItem
-        mb="30px"
-        key={text}
+        mb="7px"
+        key={item}
+        colSpan={{ base: 4, sm: 4, md: 2, lg: 2 }}
       >
-        {text}
+        <ListItem
+          title1="COLLEGIATE ARC HOODIE"
+          title2="METAFACTORY"
+          assetUrl={`/test_assets/list_items/${item}.png`}
+          redirectPath={`/closet/item_detail/${item}`}
+        />
       </GridItem>
       )}
     </Grid>
---
+++ components/closet/shared/ListItem.tsx
@@ -0,0 +1,39 @@
+/* eslint-disable prettier/prettier */
+import React from "react";
+import { VStack, Text, Box } from "@chakra-ui/react";
+import Image from "next/image";
+import { useRouter } from "next/router";
+
+const ListItem: React.FC<{
+  assetUrl: string;
+  title1: string;
+  title2: string;
+  redirectPath: string;
+}> = ({ assetUrl, title1, title2, redirectPath, ...props }) => {
+  const router = useRouter();
+  const handleClick = () => {
+    router.push(redirectPath)
+  };
+
+  return (
+    <VStack  spacing="0px" m="5px">
+      <Box border="1px" p="1px">
+        <Image
+          src={assetUrl}
+          alt=""
+          width="163px"
+          height="163px"
+          onClick={handleClick}
+          {...props}
+        />
+      </Box>
+      <Text fontFamily="body_regular" fontWeight="400" fontSize="11px">
+        {title1}
+      </Text>
+      <Text fontFamily="body_regular" fontWeight="400" fontSize="8px">
+        {title2}
+      </Text>
+    </VStack>
+  );
+}
+export default ListItem;

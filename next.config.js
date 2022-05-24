836948b1da189df98c66fb7309d89e73318a7f47
xivanc <xivanc01@gmail.com>
2022-05-24 11:38:39 +0200

    update dependencies with three fiber

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withTM = require("next-transpile-modules")(["three"]);
+
 module.exports = {
   reactStrictMode: true,
+  ...withTM(),
 };

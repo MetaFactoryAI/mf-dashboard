d0ca312de8e279f986fa3ad30d4109f656a1c4c9
xivanc <xivanc01@gmail.com>
2022-06-18 13:54:25 +0200

    fx

--- pages/api/nfts/[Id].ts
+++
@@ -1,9 +0,0 @@
-// @ts-ignore
-export default async function handler(req, res) {
-  const { id } = req.query;
-  console.log(`https://mf-services.vercel.app/api/nftMetadata/${id}`);
-  const data = await fetch(`https://mf-services.vercel.app/api/nftMetadata/${id}`);
-  const result = await data.json();
-
-  res.status(200).json(result);
-}

fefd775c896359c640b0c70edeb60a0976eac307
xivanc <xivanc01@gmail.com>
2022-06-18 13:57:08 +0200

    fx

--- pages/api/nfts/[pid].ts
+++ pages/api/nfts/[pid].ts
@@ -1,6 +1,6 @@
 // @ts-ignore
 export default async function handler(req, res) {
-  const { pid } = req.query
+  const { pid } = req.query;
   console.log(`https://mf-services.vercel.app/api/nftMetadata/${pid}`);
   const data = await fetch(`https://mf-services.vercel.app/api/nftMetadata/${pid}`);
   const result = await data.json();

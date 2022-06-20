f1980c0b1c906333f11c3e0395b8662f743bcdba
xivanc <xivanc01@gmail.com>
2022-06-20 13:01:50 +0200

    fix wearable items layout

--- components/closet/ListItems/index.tsx
+++ components/closet/ListItems/index.tsx
@@ -14,7 +14,7 @@ const ListItems: React.FC<{
       { items.map((item, index) =>
       <GridItem
         key={item.nft_token_id}
-        colSpan={{ base: 6, sm: 6, md: 2, lg: 2 }}
+        colSpan={{ base: 6, sm: 6, md: 4, lg: 3 }}
         borderRight={{ base: calculateMobileBorder(index), sm: calculateMobileBorder(index), md: "1px", lg: "1px" }}
         borderBottom="1px"
       >

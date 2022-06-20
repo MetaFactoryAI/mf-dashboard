2f7262a59df814e573c44584817f581a1583f073
xivanc <xivanc01@gmail.com>
2022-06-20 13:14:19 +0200

    fix Connect layout

--- components/profile/Connect.tsx
+++ components/profile/Connect.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable prettier/prettier */
-import { VStack, Text, Box } from "@chakra-ui/react";
+import { VStack, Text, Center, Flex, Box } from "@chakra-ui/react";
 import Button from "@/components/atoms/Button";
 import type { NextPage } from "next";
 import Image from "next/image";
@@ -17,28 +17,32 @@ const Connect: NextPage = () => {
   }, []);
 
   return (
-    <VStack p="4">
-      <Box mb="8">
-        <Image src="/w95_misc.png" alt="" width="108" height="82" />
-      </Box>
-      <Box maxWidth="266px" pb="40px">
-        <Text fontFamily="heading" letterSpacing="-0.02em" lineHeight="35px" fontWeight="700" fontSize="29px" mx="15px" paddingBottom="0px" textAlign="center">
-          MetaFactory Wearables OS
-        </Text>
-      </Box>
-      <Box width="100%" maxWidth="330px" pb="40px">
-        <Button
-          handleClickCallback={() => {
-            const connector = isDesktopScreen ? connectors[0] : connectors[1]
-            connect(connector)
-          }}
-          height="40px"
-          width="100%"
-          backgroundColor="yellow"
-          title="CONNECT WALLET"
-        />
-      </Box>
-    </VStack>
+    <Flex flex="1" flexDirection="row" alignContent="center" justifyItems="center">
+      <Center>
+        <VStack px="88px" py="58px">
+          <Box mb="8">
+            <Image src="/w95_misc.png" alt="" width="108" height="82" />
+          </Box>
+          <Box maxWidth="266px" pb="40px">
+            <Text fontFamily="heading" letterSpacing="-0.02em" lineHeight="35px" fontWeight="700" fontSize="29px" mx="15px" paddingBottom="0px" textAlign="center">
+              MetaFactory Wearables OS
+            </Text>
+          </Box>
+          <Box width="100%" maxWidth="330px" pb="40px">
+            <Button
+              handleClickCallback={() => {
+                const connector = isDesktopScreen ? connectors[0] : connectors[1]
+                connect(connector)
+              }}
+              height="40px"
+              width="100%"
+              backgroundColor="yellow"
+              title="CONNECT WALLET"
+            />
+          </Box>
+        </VStack>
+      </Center>
+  </Flex>
   );
 };
 

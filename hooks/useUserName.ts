c268ba694083b5f74c1f7990fa333139538e20e4
xivanc <xivanc01@gmail.com>
2022-07-28 15:45:19 +0200

    fix userName

--- hooks/useUserName.ts
+++ hooks/useUserName.ts
@@ -1,15 +1,16 @@
-import { useMemo } from "react";
+import { useState, useEffect } from "react";
 import { useAccount, useEnsName } from "wagmi";
 import { formatAddress } from "@/utils/presentationHelper";
 
 const useUserName = () => {
   const { data: account } = useAccount();
   const { data: ensName } = useEnsName({ address: account?.address });
+  const [userName, setUserName] = useState<string | null>(null);
 
-  const userName = useMemo(
-    () => ensName ?? formatAddress(account?.address),
-    [account?.address, ensName],
-  );
+  useEffect(() => {
+    const currentName = ensName ?? formatAddress(account?.address);
+    setUserName(currentName);
+  }, [account?.address, ensName]);
 
   return userName;
 };
